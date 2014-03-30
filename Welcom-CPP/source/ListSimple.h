7fd4380f1a1605e743e185f8208e87514eaabaef
arturo <arturo@arturo-Inspiron-3521>
2014-03-30 11:52:09 -0600

    Estructuras fundamentales del proyecto.

---
+++ Welcom-CPP/source/ListSimple.h
@@ -0,0 +1,18 @@
+/*
+ * ListSimple.h
+ *
+ *  Created on: 30/03/2014
+ *      Author: Arturo Mora
+ */
+
+#ifndef LISTSIMPLE_H_
+#define LISTSIMPLE_H_
+
+class ListSimple
+{
+public:
+    ListSimple();
+    virtual ~ListSimple();
+};
+
+#endif /* LISTSIMPLE_H_ */
